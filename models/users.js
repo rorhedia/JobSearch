0b281fcdf6fe8771f94f84804bfe9a5c62a94ae5
Daniel Heredia <rorhedia@gmail.com>
2022-05-03 12:09:47 -0500

    Creación de schema de usuarios

--- models/users.js
+++ models/users.js
@@ -1,4 +1,12 @@
 const { mongoose } = require( '../config/db' );
 
-// Crear modelo de usuarios
+const Schema = mongoose.Schema;
 
+const userSchema = new Schema( {
+    name    : String,
+    email   : String,
+    password: String
+} );
+
+const UserModel = mongoose.model( 'User', userSchema );
+module.exports  = UserModel;
