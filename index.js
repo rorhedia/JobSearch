a1b3a7e6235e59ea85a1581e85350804aed7dd8e
Daniel Heredia <rorhedia@gmail.com>
2022-05-12 09:22:37 -0500

    se agregan cors

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
 const express        = require( 'express' );
+const cors           = require( 'cors' );
 const { PORT }       = require( './config' );
 const { connection } = require( './config/db' );
 
 const users = require( './routes/users' );
-const auth = require( './routes/auth' );
+const auth  = require( './routes/auth' );
 
 connection();
 const app = express();
+app.use( cors( {
+    origin: [ 'http://localhost:3000' ]
+} ) )
 app.use( express.json() );
 
 users( app );
